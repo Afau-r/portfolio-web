2fb16742247d7dcf1b78ae0854049340921d24ef
Alex Faugit config --global user.name Alex <afau2000@gmail.com>
2025-04-28 10:56:32 +0200

    Fix rutas para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/portfolio-web/',  // mi repositorio
   plugins: [react()],
-  base: '/portfolio-web',  // mi repositorio
 })
